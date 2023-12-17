ae5f4139e418bc33949cc61764e90c904c500de4
naveenkumar061 <devarashettynaveen224@gmail.com>
2023-12-17 19:01:17 +0530

    Commit Day 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// Global Variables
+let savedListArray = [];
+
+// Fetching News
 const fetchNews = async () => {
   let result = await fetch(
     "https://content.newtonschool.co/v1/pr/64e3d1b73321338e9f18e1a1/inshortsnews"
@@ -6,48 +10,57 @@ const fetchNews = async () => {
   return response;
 };
 
+// Getting Categories
 const getCategories = async () => {
   const allCategoryInfo = await fetchNews();
-  // console.log(allCategoryInfo);
   const categoryList = new Set();
   allCategoryInfo.map((item, index) => {
     categoryList.add(item.category);
   });
   const categoryListArray = Array.from(categoryList);
   categoryListArray.unshift("All");
-  const getCategoryContainer = document.getElementById("getCategories");
+  const selectorContainer = document.getElementById("selection");
   categoryListArray.map((categoryName, index) => {
     const button = document.createElement("button");
     button.classList.add("category");
     button.textContent = categoryName;
-    getCategoryContainer.appendChild(button);
+    selectorContainer.appendChild(button);
+    if (categoryName === "All") button.classList.add("active");
     button.addEventListener("click", () => {
-      document.getElementById("new").innerHTML = "";
-      filterData(categoryName);
+      document.querySelectorAll(".category").forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      button.classList.add("active");
+      document.getElementById("allNewsDetails").innerHTML = "";
+      displayNews(categoryName);
     });
   });
 };
 getCategories();
 
-const filterData = async (value) => {
+// Displaying News
+async function displayNews(value) {
   const allData = await fetchNews();
-  const container = document.getElementById("new");
+  const container = document.getElementById("allNewsDetails");
   allData.map((item, index) => {
     if (value === "All" || value === item.category) {
-      const newContainer = document.createElement("div");
-      newContainer.innerHTML = `
-        <div>
-            <p>by <span>${item.author}</span></p>
-            <p>category <span>${item.category}</span></p>
+      const contentContainer = document.createElement("div");
+      contentContainer.classList.add("content");
+      contentContainer.innerHTML = `
+      <div class="main-details">
+          <p>by <span>${item.author}</span></p>
+          <p>category <span>${item.category}</span></p>
         </div>
-        <div>
-            <p>${item.content} <span>read more.</span></p>
+        <div class="main-content">
+          <p>${item.content} <span>read more....</span></p>
         </div>
-        <div id"saved"><i class="fa-regular fa-heart"></i></div>
+        <div class="icon">
+          <i class="fa-regular fa-heart"></i>
+        </div>
       `;
-      container.appendChild(newContainer);
+      container.appendChild(contentContainer);
     }
   });
-};
+}
 
-filterData("All");
+displayNews("All");
